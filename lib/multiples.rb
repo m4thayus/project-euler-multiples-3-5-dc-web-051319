a9dd45fab0a7fa7af3497173c624b5958bea5339
Matt Williams <matt.seongjun@gmail.com>
2019-08-07 09:34:38 -0400

    Done procedural

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+
+def multiples(n)
+    i = 1
+    multi = []
+    while i < n do
+        if i % 3 == 0 || i % 5 == 0 
+            multi << i
+        end
+
+        if i < 10
+            puts "Iteration #{i}: #{multi}"
+        end
+
+        i += 1
+    end
+
+    return multi.inject{ |sum, x| sum + x}
+end
+
+puts multiples(10)
+puts multiples(1000)
